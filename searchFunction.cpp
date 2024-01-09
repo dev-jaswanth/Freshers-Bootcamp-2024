978c485fe100e4c81049583506e2b582b1c649c0
dev-jaswanth <108873022+dev-jaswanth@users.noreply.github.com>
2024-01-09 14:51:36 +0530

    Update searchFunction.cpp

--- searchFunction.cpp
+++ searchFunction.cpp
@@ -1,16 +1,37 @@
 #include <bits/stdc++.h>
 
-std::vector<int> filterFunction(const std::vector<int> &list, std::function<bool(int)> Criteria) {
-    std::vector<int> filteredArray;
+using namespace std;
+
+static bool Criteria(int element){
+    if(element>10){
+    return true;
+    }
+    return false;
+}
+
+std::vector<int> filterFunction(const vector<int> &list) {
+    std::vector<int>filteredArray;
     for(auto const &element: list){
-        if(Criteria(element))
+        if(Criteria(element)){
             filteredArray.push_back(element);
+        }
     }
     return filteredArray;
 }
 
 int main()
 {
-    std::cout<<"Hello World";
+    int size_of_Array;
+    cin>>size_of_Array;
+    
+    vector<int>list(size_of_Array);
+    
+    for(int ele=0;ele<size_of_Array;ele++){
+        cin>>list[ele];
+    }
+    vector<int>answer = filterFunction(list);
+    for(auto ele : answer){
+        cout<<ele<<" ";
+    }
     return 0;
 }
