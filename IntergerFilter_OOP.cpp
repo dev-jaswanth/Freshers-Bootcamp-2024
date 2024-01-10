5ef35791e08a8103fd7bf1dc0b92be3875c93e03
dev-jaswanth <108873022+dev-jaswanth@users.noreply.github.com>
2024-01-10 15:35:57 +0530

    Update IntergerFilter_OOP.cpp

--- IntergerFilter_OOP.cpp
+++ IntergerFilter_OOP.cpp
@@ -1,44 +1,94 @@
 #include<bits/stdc++.h>
 using namespace std;
-class IntegerFilter {
-public:
-     IntegerFilter(const vector<int>& numbers) : numbers(numbers) {}
 
-    vector<int> filter(const function<bool(int)>& greaterThanTen) {
+#include<bits/stdc++.h>
+using namespace std;
+
+class GreaterThanStrategy {
+private:
+    int threshold;
+
+public:
+    void setThreshold(int value) {
+        this->threshold = value;
+    }
+
+    bool invoke(int num) {
+        return num > threshold;
+    }
+};
+
+class IntegerListFilterController {
+private:
+    GreaterThanStrategy GTS;
+
+public:
+    vector<int> filter(const vector<int>& numbers) {
+        GTS.setThreshold(10);
         vector<int> result;
         for (const auto& num : numbers) {
-            if (greaterThanTen(num)) {
+            if (GTS.invoke(num)) {
                 result.push_back(num);
             }
         }
         return result;
     }
-
-    void printToConsole(const vector<int>& vec) const {
-        for (const auto& item : vec) {
-            cout << item << ", ";
-        }
-        cout << std::endl;
-    }
-
-    static function<bool(int)> greaterThanTen() {
-        return [](int num) { return num > 10; };
-    }
-
-private:
-    vector<int> numbers;
 };
 
 int main() {
     vector<int> numbers = {5, 12, 8, 15, 3, 20};
 
-    IntegerFilter integerFilter(numbers);
+    IntegerListFilterController integerFilterController;
 
-    auto greaterThanTen = IntegerFilter::greaterThanTen();
+    vector<int> numbersGreaterThanTen = integerFilterController.filter(numbers);
 
-    vector<int> numbersGreaterThanTen = integerFilter.filter(greaterThanTen);
-
-    integerFilter.printToConsole(numbersGreaterThanTen);
+    for (const auto& item : numbersGreaterThanTen) {
+        cout << item << " ";
+    }
+    cout << endl;
 
     return 0;
 }
+
+// class IntegerFilter {
+// public:
+//      IntegerFilter(const vector<int>& numbers) : numbers(numbers) {}
+
+//     vector<int> filter(const function<bool(int)>& greaterThanTen) {
+//         vector<int> result;
+//         for (const auto& num : numbers) {
+//             if (greaterThanTen(num)) {
+//                 result.push_back(num);
+//             }
+//         }
+//         return result;
+//     }
+
+//     void printToConsole(const vector<int>& vec) const {
+//         for (const auto& item : vec) {
+//             cout << item << ", ";
+//         }
+//         cout << std::endl;
+//     }
+
+//     static function<bool(int)> greaterThanTen() {
+//         return [](int num) { return num > 10; };
+//     }
+
+// private:
+//     vector<int> numbers;
+// };
+
+// int main() {
+//     vector<int> numbers = {5, 12, 8, 15, 3, 20};
+
+//     IntegerFilter integerFilter(numbers);
+
+//     auto greaterThanTen = IntegerFilter::greaterThanTen();
+
+//     vector<int> numbersGreaterThanTen = integerFilter.filter(greaterThanTen);
+
+//     integerFilter.printToConsole(numbersGreaterThanTen);
+
+//     return 0;
+// }
